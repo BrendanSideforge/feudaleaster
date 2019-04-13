c1a5152aedc8ecbfc1b7dc502c7ec4b7679891ef
brendansides12 <46138190+brendansides12@users.noreply.github.com>
2019-04-12 23:12:22 -0500

    Update shop.py

--- Feudal Easter Bot/cogs/shop.py
+++ Feudal Easter Bot/cogs/shop.py
@@ -6,7 +6,43 @@ from collections import defaultdict
 import random
 
 class Shop(commands.Cog):
+    def __init__(self, bot):import discord 
+from discord.ext import commands 
+import pymongo 
+from pymongo import MongoClient as mcl
+from collections import defaultdict
+import random
+
+class Shop(commands.Cog):
     def __init__(self, bot):
+        self.bot = bot 
+        self.client = mcl("mongodb://Brendan:BS103261@ds137102.mlab.com:37102/feudaleaster")
+        self.db = self.client["feudaleaster"]
+        self.col = self.db["users"]
+        self.data = self.col.find_one()
+        self.channels = [
+            422566574541766656,
+            426515636571734016
+        ]
+
+    @commands.command()
+    async def shop(self, ctx):
+        if not ctx.channel.id in self.channels:
+            return
+        if ctx.author.id in self.bot.blacklisted:
+            return await ctx.author.send(f"Nope, you're not gonna use any commands.")
+        embed = discord.Embed(color=self.bot.embed_colour)
+        embed.title = "Egg Shop"
+        embed.add_field(name=f"{self.bot.mutliplier} Egg Multiplier", value=f"""
+        **Item ID:** 4320
+        **Cost:** 5 eggs
+        **Description:** Everytime you catch an egg you get 2x the amount of eggs.
+        """)
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Shop(bot))
+
         self.bot = bot 
         self.client = mcl("mongodb://Brendan:BS103261@ds137102.mlab.com:37102/feudaleaster")
         self.db = self.client["feudaleaster"]
