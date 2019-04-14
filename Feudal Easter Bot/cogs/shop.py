3fd7e41a1d440cba61fcabfb29d16162ab954118
brendansides12 <46138190+brendansides12@users.noreply.github.com>
2019-04-13 19:39:35 -0500

    Update shop.py

--- Feudal Easter Bot/cogs/shop.py
+++ Feudal Easter Bot/cogs/shop.py
@@ -6,14 +6,6 @@ from collections import defaultdict
 import random
 
 class Shop(commands.Cog):
-    def __init__(self, bot):import discord 
-from discord.ext import commands 
-import pymongo 
-from pymongo import MongoClient as mcl
-from collections import defaultdict
-import random
-
-class Shop(commands.Cog):
     def __init__(self, bot):
         self.bot = bot 
         self.client = mcl("mongodb://Brendan:BS103261@ds137102.mlab.com:37102/feudaleaster")
@@ -38,33 +30,43 @@ class Shop(commands.Cog):
         **Cost:** 5 eggs
         **Description:** Everytime you catch an egg you get 2x the amount of eggs.
         """)
+        embed.set_footer(text="Use the command - $buy <item_id/item_name> - to buy a item!")
         await ctx.send(embed=embed)
-
-def setup(bot):
-    bot.add_cog(Shop(bot))
-
-        self.bot = bot 
-        self.client = mcl("mongodb://Brendan:BS103261@ds137102.mlab.com:37102/feudaleaster")
-        self.db = self.client["feudaleaster"]
-        self.col = self.db["users"]
-        self.data = self.col.find_one()
-        self.channels = [
-            422566574541766656,
-            426515636571734016
-        ]
 
     @commands.command()
-    async def shop(self, ctx):
-        if not ctx.channel.id in self.channels:
-            return
-        embed = discord.Embed(color=self.bot.embed_colour)
-        embed.title = "Egg Shop"
-        embed.add_field(name=f"{self.bot.mutliplier} Egg Multiplier", value=f"""
-        **Item ID:** 4320
-        **Cost:** 5 eggs
-        **Description:** Everytime you catch an egg you get 2x the amount of eggs.
-        """)
-        await ctx.send(embed=embed)
+    async def buy(self, ctx, *, item = None):
+        self.data = self.col.find_one()
+        if not item:
+            return await ctx.send(f"{self.bot.x_mark} Please use the command **$shop** to see all of the items.")
+        if not ctx.author.id in self.data:
+            document = {
+                {"auth": True}, {
+                    "$set": {str(ctx.author.id):{
+                        "eggs": 0,
+                        "currency": 0,
+                        "items": []
+                    }}
+                }}
+            self.col.update_one(document)
+            return await ctx.send(f"{self.bot.x_mark} You do not have enough eggs to buy any item.")
+        if item == 4320:
+            eggs = self.data[str(ctx.author.id)]["eggs"]
+            if eggs > 5:
+                return await ctx.send(f"{self.bot.x_mark} Very sorry **{ctx.author.name}**! You do not have enough eggs to buy the **Egg Multiplier**! Your current eggs are **{eggs}/5**.")
+            else:
+                items = self.data[str(ctx.author.id)]["items"]
+                document = {
+                    {"auth": True},
+                    {
+                        "$set": {str(ctx.author.id):{
+                            "eggs": eggs,
+                            "currency": self.data[str(ctx.author.id)]["currency"],
+                            "items": items.append("Egg Multiplier")
+                        }}
+                    }
+                }
+                self.col.update_one(document)
+                await ctx.send(f"{self.bot.check_mark} Yay! You have bought the {self.bot.mutliplier} **Egg Multiplier**! You will now get 2x the amount of eggs!")
 
 def setup(bot):
     bot.add_cog(Shop(bot))
