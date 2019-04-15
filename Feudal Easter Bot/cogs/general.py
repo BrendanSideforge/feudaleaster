673d1fa7afdfd8aa2f7b0058bed461e8c98ad1cc
brendansides12 <46138190+brendansides12@users.noreply.github.com>
2019-04-15 07:38:27 -0500

    Update general.py

--- Feudal Easter Bot/cogs/general.py
+++ Feudal Easter Bot/cogs/general.py
@@ -18,19 +18,22 @@ class General(commands.Cog):
         self.data = self.col.find_one()
         if not ctx.author.id in self.bot.devs:
             return 
-        users = 0
-        print(self.data)
-        for user in self.data:
-            users += 1
+        users_count = 0
+        server_users = self.data 
+        users = [x for x in server_users if len(x) == 18]
+        for user in users:
+            print(user)
+            users_count += 1
             document = {"$set":{str(user):{
-                "eggs": self.data[user]["eggs"],
-                "currency": self.data[user]["currency"],
-                "items": self.data[user]["items"],
-                "egg-streak": 0,
-                "loo-streak": 0
+                "eggs": self.data[str(user)]["eggs"],
+                "currency": self.data[str(user)]["currency"],
+                "items": self.data[str(user)]["items"],
+                "egg-streak": self.data[str(user)]["egg-streak"],
+                "loo-streak": self.data[str(user)]["loot-streak"],
+                "animals": []
             }}}
             self.col.update_one({"auth": True}, document)
-        await ctx.send(f"{self.bot.check_mark} I have changed all of the users in the database. I changed **{users}** users successfully.")
+        await ctx.send(f"{self.bot.check_mark} I have changed all of the users in the database. I changed **{users_count}** users successfully.")
 
 def setup(bot):
     bot.add_cog(General(bot))
